a21f30e1105308767d35889765c077a05541b32c
Valentin Kovbasiuk <valentin.mail@gmail.com>
2018-11-16 18:09:33 +0200

    button to load more cats

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-document.querySelector(".js-add-cat").addEventListener("click", addCat);
+var pageNum = 1;
 
 let catTemplate = document.importNode(
   document.querySelector("#cat").content,
   true
 );
+
 let gridContainer = document.querySelector(".grid__container");
+
+document.querySelector(".js-add-cat").addEventListener("click", loadMoreCats);
 
 function addCat(element) {
   let catFromTemplate = catTemplate.cloneNode(true);
@@ -15,12 +18,26 @@ function addCat(element) {
   gridContainer.appendChild(catFromTemplate);
 }
 
-fetch("https://ma-cats-api.herokuapp.com/api/cats")
-  .then(response => response.json())
-  .then(data => data.cats)
-  .then(catArray =>
-    catArray.forEach(cat => {
-      console.log(cat);
-      addCat(cat);
-    })
-  );
+function loadCats(page = 1, per_page = 50) {
+  fetch(
+    `https://ma-cats-api.herokuapp.com/api/cats?page=${page}&per_page=${per_page}`
+  )
+    .then(response => response.json())
+    .then(data => data.cats)
+    .then(catArray =>
+      catArray.forEach(cat => {
+        addCat(cat);
+      })
+    );
+}
+
+function loadMoreCats(page, per_page) {
+  console.log(pageNum);
+  pageNum++;
+  console.log(pageNum);
+  loadCats(pageNum, per_page);
+}
+
+(function() {
+  loadCats();
+})();
