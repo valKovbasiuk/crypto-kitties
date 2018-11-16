b5ba35282bd4c7d1632cf9eb94a1aec6ed00f354
Valentin Kovbasiuk <valentin.mail@gmail.com>
2018-11-16 17:39:40 +0200

    fetch first page of cats

--- app.js
+++ app.js
@@ -1,43 +1,3 @@
-// fetch("https://ma-cats-api.herokuapp.com/api/cats")
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(myJson) {
-//     console.log(JSON.parse(myJson));
-//   }).catch(error) {console.log(error)};
-
-var cats = (function getCats() {
-  fetch("https://ma-cats-api.herokuapp.com/api/cats")
-    .then(resp => resp.json())
-    .then(function(data) {
-      return data;
-    });
-})();
-console.log(cats);
-
-let catArray = [
-  {
-    img:
-      "https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1082340.svg",
-    price: 0.1234
-  },
-  {
-    img:
-      "https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1075399.svg",
-    price: 0.2222
-  },
-  {
-    img:
-      "https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1072111.svg",
-    price: 0.3333
-  },
-  {
-    img:
-      "https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/493457.svg",
-    price: 0.4444
-  }
-];
-
 document.querySelector(".js-add-cat").addEventListener("click", addCat);
 
 let catTemplate = document.importNode(
@@ -46,20 +6,21 @@ let catTemplate = document.importNode(
 );
 let gridContainer = document.querySelector(".grid__container");
 
-var i = 0;
-
-function addCat() {
+function addCat(element) {
   let catFromTemplate = catTemplate.cloneNode(true);
 
-  catFromTemplate.querySelector(".item__image").src = catArray[i].img;
-  catFromTemplate.querySelector(".item__price-value").innerHTML =
-    catArray[i].price;
+  catFromTemplate.querySelector(".item__image").src = element.img_url;
+  catFromTemplate.querySelector(".item__price-value").innerHTML = element.price;
 
   gridContainer.appendChild(catFromTemplate);
-
-  i++;
-
-  if (i === catArray.length) {
-    i = 0;
-  }
 }
+
+fetch("https://ma-cats-api.herokuapp.com/api/cats")
+  .then(response => response.json())
+  .then(data => data.cats)
+  .then(catArray =>
+    catArray.forEach(cat => {
+      console.log(cat);
+      addCat(cat);
+    })
+  );
